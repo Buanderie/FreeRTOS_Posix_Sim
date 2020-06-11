f5dd98e82c4335d882f66aec2fbb8d006c137793
Nicolas <nicolas.said@gmail.com>
2020-06-11 15:38:28 +0200

    dfsds

--- FreeRTOS_Kernel/portable/GCC/Posix/include/portmacro.h
+++ FreeRTOS_Kernel/portable/GCC/Posix/include/portmacro.h
@@ -1,53 +1,14 @@
 /*
-	FreeRTOS.org V5.2.0 - Copyright (C) 2003-2009 Richard Barry.
-
-	This file is part of the FreeRTOS.org distribution.
-
-	FreeRTOS.org is free software; you can redistribute it and/or modify it
-	under the terms of the GNU General Public License (version 2) as published
-	by the Free Software Foundation and modified by the FreeRTOS exception.
-
-	FreeRTOS.org is distributed in the hope that it will be useful,	but WITHOUT
-	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-	FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-	more details.
-
-	You should have received a copy of the GNU General Public License along
-	with FreeRTOS.org; if not, write to the Free Software Foundation, Inc., 59
-	Temple Place, Suite 330, Boston, MA  02111-1307  USA.
-
-	A special exception to the GPL is included to allow you to distribute a
-	combined work that includes FreeRTOS.org without being obliged to provide
-	the source code for any proprietary components.  See the licensing section
-	of http://www.FreeRTOS.org for full details.
-
-
-	***************************************************************************
-	*                                                                         *
-	* Get the FreeRTOS eBook!  See http://www.FreeRTOS.org/Documentation      *
-	*                                                                         *
-	* This is a concise, step by step, 'hands on' guide that describes both   *
-	* general multitasking concepts and FreeRTOS specifics. It presents and   *
-	* explains numerous examples that are written using the FreeRTOS API.     *
-	* Full source code for all the examples is provided in an accompanying    *
-	* .zip file.                                                              *
-	*                                                                         *
-	***************************************************************************
-
-	1 tab == 4 spaces!
-
-	Please ensure to read the configuration and relevant port sections of the
-	online documentation.
-
-	http://www.FreeRTOS.org - Documentation, latest information, license and
-	contact details.
-
-	http://www.SafeRTOS.com - A version that is certified for use in safety
-	critical systems.
-
-	http://www.OpenRTOS.com - Commercial support, development, porting,
-	licensing and training services.
-*/
+ * FreeRTOS Port implementation for Linux using POSIX Threads
+ *
+ * TODO Describe usage
+ *
+ * Copyright (C) 2019 Andreas Innerlohninger <innerand@nxa.at>
+ * Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * This code is subject to the terms and conditions defined at the MIT Licence
+ * which are specified at the end of this file and/or a separate license file.
+ */
 
 
 #ifndef PORTMACRO_H
@@ -57,108 +18,130 @@
 extern "C" {
 #endif
 
-/*-----------------------------------------------------------
- * Port specific definitions.
+/*
+ * Signals used by the port
  *
- * The settings in this file configure FreeRTOS correctly for the
- * given hardware and compiler.
- *
- * These settings should not be altered.
- *-----------------------------------------------------------
+ * If the application uses multiple threads this signals must be blocked and no
+ * signal handler must be installed for them. Blocking those signals ist done by
+ * vPortEarlyInit().
  */
+#define portSIGTIMER    (SIGRTMIN + 1)
+#define portSIGSUSPEND  (SIGRTMIN + 2)
+#define portSIGRESUME   (SIGRTMIN + 3)
+#define portSIGSCHEDULE (SIGRTMIN + 4)
+/*-----------------------------------------------------------*/
 
 /* Type definitions. */
-#define portCHAR		char
-#define portFLOAT		float
-#define portDOUBLE		double
-#define portLONG		int
-#define portSHORT		short
-#define portSTACK_TYPE  unsigned long
-#define portBASE_TYPE   long
+#define portSTACK_TYPE	uint32_t
+typedef portSTACK_TYPE StackType_t;
+typedef long BaseType_t;
+typedef unsigned long UBaseType_t;
 
 #if( configUSE_16_BIT_TICKS == 1 )
-	typedef unsigned portSHORT portTickType;
-	#define portMAX_DELAY ( portTickType ) 0xffff
+	typedef uint16_t TickType_t;
+	#define portMAX_DELAY ( TickType_t ) 0xffff
 #else
-	typedef unsigned portLONG portTickType;
-	#define portMAX_DELAY ( portTickType ) 0xffffffff
+	typedef uint32_t TickType_t;
+	#define portMAX_DELAY ( TickType_t ) 0xffffffffUL
+
+    #if defined(__i386__)
+	/* 32-bit tick type on a 32-bit architecture, so reads of the tick count do
+	not need to be guarded with a critical section. */
+	#define portTICK_TYPE_IS_ATOMIC 1
+    #endif
 #endif
+
+#if defined( __x86_64__)
+    #define portPOINTER_SIZE_TYPE  uint64_t
+#elif defined( __i386__ )
+    #define portPOINTER_SIZE_TYPE  uint32_t
+#else
+    #error "Pointer size type unknown"
+#endif
+
 /*-----------------------------------------------------------*/
 
 /* Architecture specifics. */
-#define portSTACK_GROWTH				( -1 )
-#define portTICK_RATE_MS				( ( portTickType ) 1000 / configTICK_RATE_HZ )
-#define portTICK_RATE_MICROSECONDS		( ( portTickType ) 1000000 / configTICK_RATE_HZ )
-#define portBYTE_ALIGNMENT				4
-#define portREMOVE_STATIC_QUALIFIER
+#define portSTACK_GROWTH			( -1 )
+#define portTICK_PERIOD_MS			( ( TickType_t ) 1000 / configTICK_RATE_HZ )
+#define portBYTE_ALIGNMENT			( 8 )
 /*-----------------------------------------------------------*/
+
+/* Early Initialization
+ *
+ * Must be called from the main thread before any RTOS functions are called
+ *
+ * @retval 0        Initialized
+ * @retval (other)  Error
+ */
+int vPortEarlyInit( void );
+
+
+#if ( configPORT_USE_REINIT != 0 )
+/* Reinitialize static variables of FreeRTOS
+ *
+ * May be useful to get a clean state at runtime (e.g. when running multiple,
+ * independent tests)
+ *
+ * Requires linkage with `freertos_linx.ld` and define `PRIVILEGED_DATA`
+ * commented out at the end of the file mpu_wrappers.h
+ */
+void vPortReinitFreeRtos(void);
+#define PRIVILEGED_DATA __attribute__((section ("free_rtos")))
+
+#endif /* configPORT_USE_REINIT */
 
 
 /* Scheduler utilities. */
-extern void vPortYieldFromISR( void );
 extern void vPortYield( void );
+#define portYIELD() vPortYield()
+#define portEND_SWITCHING_ISR( xSwitchRequired ) if( xSwitchRequired != pdFALSE ) portYIELD()
+#define portYIELD_FROM_ISR( x ) portEND_SWITCHING_ISR( x )
 
-#define portYIELD()					vPortYield()
+extern void vPortYieldWithinAPI( void );
+#define portYIELD_WITHIN_API() vPortYieldWithinAPI()
 
-#define portEND_SWITCHING_ISR( xSwitchRequired ) if( xSwitchRequired ) vPortYieldFromISR()
+/*
+ * Pauses execution off all tasks, disables systick handling
+ *
+ * May be useful to call *FromISR FreeRTOS functions outside of the RTOS context
+ * NOTE: If the scheduler hasn't been started, this function will block until it
+ *       is.
+ */
+extern void vPortPause( void );
+
+/*
+ * Resumes task execution and systick handling after vPortPause()
+ */
+extern void vPortResume ( void );
 /*-----------------------------------------------------------*/
 
-
 /* Critical section management. */
-extern void vPortDisableInterrupts( void );
-extern void vPortEnableInterrupts( void );
-#define portSET_INTERRUPT_MASK()	( vPortDisableInterrupts() )
-#define portCLEAR_INTERRUPT_MASK()	( vPortEnableInterrupts() )
-
-extern portBASE_TYPE xPortSetInterruptMask( void );
-extern void vPortClearInterruptMask( portBASE_TYPE xMask );
-
-#define portSET_INTERRUPT_MASK_FROM_ISR()		xPortSetInterruptMask()
-#define portCLEAR_INTERRUPT_MASK_FROM_ISR(x)	vPortClearInterruptMask(x)
-
+#define portSET_INTERRUPT_MASK_FROM_ISR()       0
+#define portCLEAR_INTERRUPT_MASK_FROM_ISR(x)    (void)(x)
+#define portDISABLE_INTERRUPTS()
+#define portENABLE_INTERRUPTS()
 
 extern void vPortEnterCritical( void );
 extern void vPortExitCritical( void );
-
-#define portDISABLE_INTERRUPTS()	portSET_INTERRUPT_MASK()
-#define portENABLE_INTERRUPTS()		portCLEAR_INTERRUPT_MASK()
-#define portENTER_CRITICAL()		vPortEnterCritical()
-#define portEXIT_CRITICAL()			vPortExitCritical()
+#define portENTER_CRITICAL() vPortEnterCritical()
+#define portEXIT_CRITICAL() vPortExitCritical()
 /*-----------------------------------------------------------*/
 
-/* Task function macros as described on the FreeRTOS.org WEB site. */
+/* Task function macros as described on the FreeRTOS.org WEB site.  These are
+not necessary for to use this port.  They are defined so the common demo files
+(which build with all the ports) will build. */
 #define portTASK_FUNCTION_PROTO( vFunction, pvParameters ) void vFunction( void *pvParameters )
 #define portTASK_FUNCTION( vFunction, pvParameters ) void vFunction( void *pvParameters )
+/*-----------------------------------------------------------*/
 
-#define portNOP()
+/* Task creation hook. Passes the task handle */
+void vPortSetupTCB(void * pvTaskHandle);
+#define portSETUP_TCB(pxTCB)    vPortSetupTCB((void*)pxTCB)
+/*-----------------------------------------------------------*/
 
-#define portOUTPUT_BYTE( a, b )
-
-extern void vPortForciblyEndThread( void *pxTaskToDelete );
-#define traceTASK_DELETE( pxTaskToDelete )		vPortForciblyEndThread( pxTaskToDelete )
-
-extern void vPortAddTaskHandle( void *pxTaskHandle );
-#define traceTASK_CREATE( pxNewTCB )			vPortAddTaskHandle( pxNewTCB )
-
-/* Posix Signal definitions that can be changed or read as appropriate. */
-#define SIG_SUSPEND					SIGUSR1
-#define SIG_RESUME					SIGUSR2
-
-/* Enable the following hash defines to make use of the real-time tick where time progresses at real-time. */
-#define SIG_TICK					SIGALRM
-#define TIMER_TYPE					ITIMER_REAL
-/* Enable the following hash defines to make use of the process tick where time progresses only when the process is executing.
-#define SIG_TICK					SIGVTALRM
-#define TIMER_TYPE					ITIMER_VIRTUAL		*/
-/* Enable the following hash defines to make use of the profile tick where time progresses when the process or system calls are executing.
-#define SIG_TICK					SIGPROF
-#define TIMER_TYPE					ITIMER_PROF */
-
-/* Make use of times(man 2) to gather run-time statistics on the tasks. */
-extern void vPortFindTicksPerSecond( void );
-#define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS()	vPortFindTicksPerSecond()		/* Nothing to do because the timer is already present. */
-extern unsigned long ulPortGetTimerValue( void );
-#define portGET_RUN_TIME_COUNTER_VALUE()			ulPortGetTimerValue()			/* Query the System time stats for this process. */
+void vPortCleanUpTCB(void * pvTaskHandle);
+#define portCLEAN_UP_TCB(pxTCB) vPortCleanUpTCB((void*) pxTCB)
 
 #ifdef __cplusplus
 }
@@ -166,3 +149,29 @@ extern unsigned long ulPortGetTimerValue( void );
 
 #endif /* PORTMACRO_H */
 
+
+
+/* License notice ----------------------------------------------------------- */
+
+/*
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
